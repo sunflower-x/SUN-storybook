89a9f0d438bb5016d0d2692299d5eb537fb4315b
duruo <duruo@dianjia.io>
2024-07-18 16:43:37 +0800

    feat:添加storybook

---
+++ apps/docs/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
