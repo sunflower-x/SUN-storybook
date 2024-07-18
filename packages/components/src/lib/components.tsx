080c27caa7c0e84b43b9d9e8dd609900ace37a28
duruo <duruo@dianjia.io>
2024-07-18 13:56:05 +0800

    init

---
+++ packages/components/src/lib/components.tsx
@@ -0,0 +1,11 @@
+import styles from './components.module.scss';
+
+export function Components() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Components!</h1>
+    </div>
+  );
+}
+
+export default Components;
---
+++ packages/utils/src/lib/utils.tsx
@@ -0,0 +1,11 @@
+import styles from './utils.module.scss';
+
+export function Utils() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Utils!</h1>
+    </div>
+  );
+}
+
+export default Utils;
